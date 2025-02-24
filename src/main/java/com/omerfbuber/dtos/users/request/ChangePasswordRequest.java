1a2de51d4f43f0837cd1d41376ff6a97c84164e5
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-24 22:50:03 +0300

    Add validation rules for request dtos.

--- src/main/java/com/omerfbuber/dtos/users/request/ChangePasswordRequest.java
+++ src/main/java/com/omerfbuber/dtos/users/request/ChangePasswordRequest.java
@@ -1,4 +1,24 @@
 package com.omerfbuber.dtos.users.request;
 
-public record ChangePasswordRequest(String email, String password, String newPassword, String confirmPassword) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ChangePasswordRequest(
+
+        @Email(message = "{validation.email.invalid}")
+        @NotBlank(message = "{validation.email.notBlank}")
+        String email,
+
+        @NotBlank(message = "{validation.password.notBlank}")
+        @Size(min = 6, message = "{validation.password.size}")
+        String password,
+
+        @NotBlank(message = "{validation.newPassword.notBlank}")
+        @Size(min = 6, message = "{validation.newPassword.size}")
+        String newPassword,
+
+        @NotBlank(message = "{validation.confirmPassword.notBlank}")
+        @Size(min = 6, message = "{validation.confirmPassword.size}")
+        String confirmPassword) {
 }
--- src/main/java/com/omerfbuber/dtos/users/request/UpdateUserRequest.java
+++ src/main/java/com/omerfbuber/dtos/users/request/UpdateUserRequest.java
@@ -1,10 +1,22 @@
 package com.omerfbuber.dtos.users.request;
 
+import jakarta.validation.constraints.*;
+
 import java.util.Date;
 
 public record UpdateUserRequest(
+        @Positive(message = "{validation.id.positive}")
         long id,
+
+        @NotBlank(message = "{validation.firstName.notBlank}")
+        @Size(min = 2, max = 50, message = "{validation.firstName.size}")
         String firstName,
+
+        @NotBlank(message = "{validation.lastName.notBlank}")
+        @Size(min = 2, max = 50, message = "{validation.lastName.size}")
         String lastName,
+
+        @NotNull(message = "{validation.birthDate.notNull}")
+        @Past(message = "{validation.birthDate.past}")
         Date birthDate
 ) { }
