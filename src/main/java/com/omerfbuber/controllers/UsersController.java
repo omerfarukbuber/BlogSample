01a10ad203cea4863f8e1a11a56f3d70b2abd240
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-25 17:41:51 +0300

    Refactor controller class to return verified response.

--- src/main/java/com/omerfbuber/controllers/UsersController.java
+++ src/main/java/com/omerfbuber/controllers/UsersController.java
@@ -4,6 +4,7 @@ import com.omerfbuber.dtos.users.request.ChangePasswordRequest;
 import com.omerfbuber.dtos.users.request.CreateUserRequest;
 import com.omerfbuber.dtos.users.request.UpdateUserRequest;
 import com.omerfbuber.dtos.users.response.UserResponse;
+import com.omerfbuber.extensions.ResponseEntityExtension;
 import com.omerfbuber.services.users.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -23,37 +24,37 @@ public class UsersController {
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAll() {
-        return userService.getAll();
+        return ResponseEntityExtension.okOrProblem(userService.getAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getById(@PathVariable long id) {
-        return userService.getById(id);
+        return ResponseEntityExtension.okOrProblem(userService.getById(id));
     }
 
     @GetMapping("/names")
     public ResponseEntity<List<String>> getNames() {
-        return userService.getAllFullNames();
+        return ResponseEntityExtension.okOrProblem(userService.getAllFullNames());
     }
 
     @PostMapping
     public ResponseEntity<UserResponse> create(@RequestBody @Valid CreateUserRequest request) {
-        return userService.save(request);
+        return ResponseEntityExtension.createdOrProblem(userService.save(request));
     }
 
     @PutMapping
     public ResponseEntity<Void> update(@RequestBody @Valid UpdateUserRequest request) {
-        return userService.update(request);
+        return ResponseEntityExtension.noContentOrProblem(userService.update(request));
     }
 
     @PutMapping("/change_password")
     public ResponseEntity<Void> changePassword(@RequestBody @Valid ChangePasswordRequest request) {
-        return userService.changePassword(request);
+        return ResponseEntityExtension.noContentOrProblem(userService.changePassword(request));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable long id) {
-        return userService.delete(id);
+        return ResponseEntityExtension.noContentOrProblem(userService.delete(id));
     }
 
 
