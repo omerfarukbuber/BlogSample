3a5211d636f8a3bba41bb1a50ac0f8b163ae33f0
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-25 22:45:38 +0300

    Add handling for global exceptions alongside validation exceptions

--- src/main/java/com/omerfbuber/extensions/GlobalExceptionHandler.java
+++ src/main/java/com/omerfbuber/extensions/GlobalExceptionHandler.java
@@ -1,26 +1,24 @@
 package com.omerfbuber.extensions;
 
-import com.omerfbuber.results.Error;
-import com.omerfbuber.results.Result;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        List<Error> errors = new ArrayList<>();
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleException(Exception ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        problemDetail.setTitle(ex.getClass().getSimpleName());
+        problemDetail.setDetail(ex.getMessage());
+        problemDetail.setType(URI.create("https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1"));
 
-        ex.getBindingResult().getFieldErrors().forEach(
-                error ->
-                    errors.add(Error.validation(error.getField(), error.getDefaultMessage())));
-
-        return ResponseEntity.of(CustomResults.toProblemDetail(Result.validationFailure(errors))).build();
+        return ResponseEntity.of(problemDetail).build();
     }
+
 }
