e35ccf9a6dd9dd8ac3aaa42356b261e2263b606c
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-28 00:08:23 +0300

    Create a scheduled service to delete expired refreshTokens from the database.

---
+++ src/main/java/com/omerfbuber/configurations/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.omerfbuber.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
---
+++ src/main/java/com/omerfbuber/services/refreshtoken/RefreshTokenCleanupService.java
@@ -0,0 +1,22 @@
+package com.omerfbuber.services.refreshtoken;
+
+import com.omerfbuber.repositories.refreshtoken.RefreshTokenRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
+
+@Slf4j
+public class RefreshTokenCleanupService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshTokenCleanupService(RefreshTokenRepository refreshTokenRepository) {
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    @Scheduled(cron = "0 0 3 * * *")
+    void deleteExpiredRefreshTokens() {
+        var count = refreshTokenRepository.deleteByExpiryDateLessThan(new Date());
+        log.info("Deleted {} expired refresh tokens", count);
+    }
+}
