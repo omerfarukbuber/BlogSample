ef11aba493df632de72239fd94899c9aae8bdd1d
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-27 18:07:07 +0300

    Create PasswordHasher service to hash and verify passwords.

---
+++ src/main/java/com/omerfbuber/services/shared/PasswordHasherImpl.java
@@ -0,0 +1,29 @@
+package com.omerfbuber.services.shared;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordHasherImpl implements PasswordHasher {
+    private final PasswordEncoder passwordEncoder;
+
+    public PasswordHasherImpl() {
+        this.passwordEncoder = getPasswordEncoder();
+    }
+
+    @Override
+    public String hash(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    @Override
+    public boolean verify(String password, String hashedPassword) {
+        return passwordEncoder.matches(password, hashedPassword);
+    }
+
+    @Override
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
